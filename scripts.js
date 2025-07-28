5b7e24584bd88830a00bef49f896dd859964a2d8
venkatsapbasis <venkatsapbasis22@gmail.com>
2025-07-28 15:36:47 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+// Typed.js Effect
+const typed = new Typed("#typed-text", {
+  strings: ["Venkataramana Ramireddy", "SAP BASIS Consultant", "SAP ECS (Enterprise Cloud Services) project"],
+  typeSpeed: 60,
+  backSpeed: 30,
+  loop: true
+});
+
+// AOS Initialization
+AOS.init({
+  duration: 1000,
+  once: true
+});
